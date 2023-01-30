04cd6c418f7dd8cd96b3ec229eb15d0faa609a4e
firosm07 <123958604+firosm07@users.noreply.github.com>
2023-01-30 14:19:26 +0100

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,17 @@
+basic.showString("Jeg er et som et virus ")
+basic.showLeds(`
+    . . . # .
+    . . . . #
+    # # # # .
+    . # . . .
+    . . . . #
+    `)
+for (let index = 0; index < 4; index++) {
+	
+}
+basic.showIcon(IconNames.Heart)
+basic.pause(100)
+basic.showString("Frida er emo")
+basic.forever(function () {
+	
+})
